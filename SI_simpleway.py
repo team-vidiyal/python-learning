5ae4c0dea8fe1a1a5b936a379fcdcb6310e70221
Gomsgeorgia <147894423+Gomsgeorgia@users.noreply.github.com>
2023-10-19 17:22:41 -0400

    Add files via upload

---
+++ SI_simpleway.py
@@ -0,0 +1,14 @@
+# first assignment for SI
+principal = float(input("Enter the principal amount"))
+
+Number_of_years = float(input ( "Enter the number of years for SI"))
+
+Rate_of_interest = float(input("Enter the interest"))
+
+Simple_interest = principal*Number_of_years*Rate_of_interest
+
+print (Simple_interest)
+
+
+
+
