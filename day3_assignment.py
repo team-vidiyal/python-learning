547a4fc5c583551845c4c1a8df4c329fd6ee8e20
Gomsgeorgia <147894423+Gomsgeorgia@users.noreply.github.com>
2023-10-27 22:22:08 -0400

    Add files via upload

---
+++ day3_assignment.py
@@ -0,0 +1,10 @@
+# get input from the user
+
+enter_a_string= str(input("enter a string:"))
+print(enter_a_string)
+
+enter_a_integer=int(input("enter an integer:"))
+print(enter_a_integer)
+
+count=enter_a_integer
+print ((enter_a_string+' ' )*count)
