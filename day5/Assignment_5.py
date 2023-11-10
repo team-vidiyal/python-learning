e9b380f97ec50250f043295acfb44593029f4ae3
Deepa Muthuswamy <mdeepamca>
2023-11-09 19:22:41 -0500

    day 5 assignment

--- day5/Assignment_5.py
+++ day5/Assignment_5.py
@@ -1,7 +1,11 @@
+#####Accept a single character from user. If it's a vowel print it's a vowel, otherwise print consonant.
+#####E.g.  User input:   e   Output: 'e' is a vowel
+
 user_option = input("Enter a alphabet : ")
+
 ####option1-----------------------------
-if len(user_option)>1:
-    print("please enter a single character only")
+if len(user_option)>1 or not user_option.isalpha():
+    print("please enter a single Alphabet only")
 else:
     print(f"Option 1 \n\t User Input : {user_option}")
     if user_option == "a" or user_option =="e" or user_option == "i" or user_option == "o" or user_option == "u" :
@@ -12,8 +16,8 @@ else:
 #option2----------------------------------
 
 vowels = ["a","e","i","o","u"]
-if len(user_option)>1:
-    print("please enter a single character only")
+if len(user_option)>1  or not user_option.isalpha():
+    print("please enter a single Alphabet only")
 else:
     print(f"Option 2 \n\t User Input : {user_option}")
     if user_option in vowels:
@@ -21,7 +25,7 @@ else:
     else:
         print(f"\t Output : '{user_option}' is consonant")
 
-#option3
+#option3 ------------------------------------------+
     print(f"Option 3 \n\t User Input : {user_option}")
     if user_option in 'aeiou':
         print(f"\t Output : '{user_option}' is vowel")
