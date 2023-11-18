3cf9a1fda2106667b678d11c4bdcf851ff40d65d
Sivark2022 <114113647+Sivark2022@users.noreply.github.com>
2023-11-18 09:33:07 -0500

    day6-assignment

---
+++ day6/tuples_sets_example.py
@@ -0,0 +1,12 @@
+#str, bool, floor, complex
+#list, tuples, set
+week_days = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' )
+print(week_days)
+
+#list - []
+#tuple - ()
+#set = {}
+numbers = [[1,2,3.0], ['Four','Hi']]
+print(numbers[1][1])
+states = {'Tamilnadu', 'Kerala', 'Karnataka', 'Kerala', 'Tamilnadu'}
+print(states)
