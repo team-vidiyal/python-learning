97378baa1712e4368faff44142dad7f6a280ddad
Deepa Muthuswamy <mdeepamca>
2023-12-02 09:38:08 -0500

    Assignment

--- Assignment/Assignment6.py
+++ Assignment/Assignment6.py
@@ -9,8 +9,8 @@ value2=int((input("Enter a value with length 3 digit: ")))
 if len(str(value1)) == 3 and len(str(value2)) == 3 :
     print(f"User Input \n\t value 1 = {value1} \n\t value 2 ={value2}")
     result = []
-    for number in range(value1, value2):
-        if (number % 2) == 0:
+    for number in range(value1, value2+1):
+        if (number % 7) == 0:
             result.append(number)
     print(f" Numbers divisible by 7 between {value1} and {value2} = {result}")
 else:
@@ -18,4 +18,4 @@ else:
     sys.exit()
 
 
-    f
+
--- Assignment/Assignment7.py
+++ Assignment/Assignment7.py
@@ -1,10 +1,14 @@
 # Given a dictionary {2:12, 3:15, 5:20, 7:24},Write a program to create a list with sum of key value pairs.Output:[14, 18, 25, 31]
 
 dict = {2:12, 3:15, 5:20, 7:24}
+print(dict[2])
+
 print(f"Dictionary : {dict}\n")
 result=[]
-for key, value in dict.items():
-    print(f"Keys: {key} and Value: {value} = Result :{key + value}")
-    result.append(key + value)
+# for key,value  in dict.items():
+#     print(f"Keys: {key} and Value: {value} = Result :{key + value}")
+#     result.append(key + value)
+for key in dict.keys():
+    result.append(key + dict[key])
 
 print(f"Result: {result}")
