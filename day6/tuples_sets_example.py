b1f0beef5a4b4615d391b07904074db626b98292
JawaharElango <jawaharsp@gmail.com>
2023-12-08 20:31:07 -0500

    Day 7- while loop, break, continue and list comprehension

--- day6/tuples_sets_example.py
+++ day6/tuples_sets_example.py
@@ -1,8 +1,8 @@
 #str, bool, float, complex
 # list, tuples, set
-week_end = (2,)
-
-print(type(week_end))
+# week_end = (2,)
+#
+# print(type(week_end))
 
 # list - []
 # tuple - () - Immutable collection, can't add or modify. But allow duplicates
@@ -10,9 +10,10 @@ print(type(week_end))
 
 
 
-states = ["Tamil Nadu", 'Karnataka', 'Kerala', 'Kerala', 'Tamil Nadu']
 
-states_without_duplicates = tuple(states)
+states = {"Tamil Nadu", 'Karnataka', 'Kerala', 'Kerala', 'Tamil Nadu'}
+
+# states_without_duplicates = set(states)
 
 
-print(states_without_duplicates)
+print(states)
