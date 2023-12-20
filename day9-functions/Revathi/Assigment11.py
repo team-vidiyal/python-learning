c8662a0301d96ad3089e841e9dc9ec616f598dbd
Revathi <revathisriganesh22@gmail.com>
2023-12-19 19:18:28 -0500

    Assigment11

--- day9-functions/Revathi/Assigment11.py
+++ day9-functions/Revathi/Assigment11.py
@@ -1,3 +1,18 @@
-def revathifun():
-    print("enter the class:")
-revathifun()
+#create a function that accepts two string parameters "s" &"t" return true if "s" is anagram of "t" and false otherwise
+
+
+def string_parameters(s1,s2):
+
+    if (sorted(s1) == sorted(s2)):  #sorted is shorting into assending order
+        return True
+    else:
+        return False
+
+s1 = "heart"
+s2 = "earth"
+print(sorted(s1))
+print(sorted(s2))
+print(string_parameters(s1,s2))
+
+#print("The strings are anagrams.")
+#print("The strings aren't anagrams.")
